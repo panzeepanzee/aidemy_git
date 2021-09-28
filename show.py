42b46ee7d340a7efa60928561a99d4f401d985e0
Seiya Kurihara <hiai3340@gmil.com>
2021-09-28 21:56:57 +0900

    add python program

--- show.py
+++ show.py
@@ -3,7 +3,7 @@
 
 import random
 
-with open("./hyakunin.txt", encoding="utf-8") as f:
+with open("./aidemy_git/hyakunin.txt", encoding="utf-8") as f:
     wakas = [s.strip() for s in f.readlines()]
 
 print(wakas[random.randrange(len(wakas))])
